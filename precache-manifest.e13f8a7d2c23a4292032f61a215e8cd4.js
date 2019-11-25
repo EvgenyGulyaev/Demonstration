845684ee247ad0eca758f89aaf3bb1787cb4dbd8
Evgeny Gulyaev <wardercompany@gmail.com>
2019-11-25 14:08:10 +0400

    Updates

---
+++ precache-manifest.e13f8a7d2c23a4292032f61a215e8cd4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a0e747ca43d67e3ff063b5c9e95707",
+    "url": "/Demonstration/index.html"
+  },
+  {
+    "revision": "29af35f8acc338085418",
+    "url": "/Demonstration/static/js/2.9fca9442.chunk.js"
+  },
+  {
+    "revision": "1df9f9d5dece2dc36170",
+    "url": "/Demonstration/static/js/main.0b4bb6af.chunk.js"
+  },
+  {
+    "revision": "225d848e467337c54693",
+    "url": "/Demonstration/static/js/runtime-main.24379865.js"
+  }
+]);
